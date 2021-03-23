b195a2cdd5fefb81a4151fff09ce30167a677e07
crelle <383362161@qq.com>
2021-03-23 17:03:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="nav">
+      <span>家庭百科系统</span>
+      <!-- <router-link to="/">家庭百科系统</router-link>|
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">
+                <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          router
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+              <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+
+//  export default {
+    //   data() {
+    //     return {
+    //       tableData: [{
+    //         date: '2016-05-02',
+    //         name: '王小虎',
+    //         address: '上海市普陀区金沙江路 1518 弄'
+    //       }, {
+    //         date: '2016-05-04',
+    //         name: '王小虎',
+    //         address: '上海市普陀区金沙江路 1517 弄'
+    //       }, {
+    //         date: '2016-05-01',
+    //         name: '王小虎',
+    //         address: '上海市普陀区金沙江路 1519 弄'
+    //       }, {
+    //         date: '2016-05-03',
+    //         name: '王小虎',
+    //         address: '上海市普陀区金沙江路 1516 弄'
+    //       }]
+    //     }
+    //   }
+    // }
+</script>
