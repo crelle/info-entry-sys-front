81b1765b37f971b556fb581b73c5af304928f86e
CoderOfRat <1404616446@qq.com>
2021-03-29 16:38:06 +0800

    转跳逻辑优化、新增配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -16,18 +16,18 @@ const routes = [
     component: ()=> import('../views/Login.vue')
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: '/sys',
+    name: 'Sys',
+    component: Home,
+    children: [
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('../views/About.vue')
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,46 +14,26 @@
             <el-submenu index="1">
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>功能页面</span>
               </template>
               <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="/home">主页</el-menu-item>
-                <el-menu-item index="/about">关于</el-menu-item>
+                <template slot="title">主要内容</template>
+                <el-menu-item index="/sys">主 页</el-menu-item>
+                <el-menu-item index="/sys/about">关 于</el-menu-item>
               </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
             </el-submenu>
-            <el-menu-item index="2">
+            <el-menu-item index="/rights">
               <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
+              <span slot="title">权限分配</span>
             </el-menu-item>
-            <el-menu-item index="2">
+            <el-menu-item index="/echarts">
               <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
+              <span slot="title">实时图表</span>
             </el-menu-item>
           </el-menu>
         </el-aside>
       </el-scrollbar>
-
-      <el-main> 我是主体内容 </el-main>
+      <el-main><router-view /></el-main>
     </el-container>
   </div>
 </template>
