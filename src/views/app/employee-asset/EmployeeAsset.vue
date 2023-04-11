187c4a741d07c3492f616d634413fb00ee4550b4
crelle <383362161@qq.com>
2023-04-11 17:16:22 +0800

    员工资产页面

--- src/views/app/employee-asset/EmployeeAsset.vue
+++ src/views/app/employee-asset/EmployeeAsset.vue
@@ -1,11 +1,109 @@
 <template>
   <div>
-    <p>12345wertyu</p>
+    <el-card>
+      <el-form :inline="true" :model="asset" class="demo-form-inline">
+        <el-form-item label="使用人">
+          <el-input v-model="asset.employeeId" placeholder="使用人"></el-input>
+        </el-form-item>
+        <el-form-item label="资产编号">
+          <el-input v-model="asset.assetNo" placeholder="资产编号"></el-input>
+        </el-form-item>
+        <el-form-item label="资产序列号">
+          <el-input
+            v-model="asset.assetSerialNumber"
+            placeholder="资产序列号"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="资产名称">
+          <el-input v-model="asset.assetName" placeholder="资产名称"></el-input>
+        </el-form-item>
+        <el-form-item label="登记时间">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="asset.createTime"
+            style="width: 100%"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item label="是否启用">
+          <el-select v-model="asset.enabled" placeholder="是否启用">
+            <el-option label="启用" value="1"></el-option>
+            <el-option label="未启用" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onCreate">新增</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onReset">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card>
+      <el-table :data="assets" stripe style="width: 100%">
+        <el-table-column prop="employeeName" label="使用人" width="180">
+        </el-table-column>
+        <el-table-column prop="assetNo" label="资产编号" width="180">
+        </el-table-column>
+        <el-table-column
+          prop="assetSerialNumber"
+          label="资产序列号"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column prop="createTime" label="创建时间" width="180">
+        </el-table-column>
+        <el-table-column prop="createBy" label="创建人" width="180">
+        </el-table-column>
+        <el-table-column prop="updateTime" label="更新时间" width="180">
+        </el-table-column>
+        <el-table-column prop="updateBy" label="更新人" width="180">
+        </el-table-column>
+        <el-table-column prop="enabled" label="是否启用" width="180">
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      asset: {
+        employeeId: "",
+        assetNo: "",
+        assetSerialNumber: "",
+        assetName: "",
+        createTime: new Date(),
+        enabled: "1",
+      },
+      assets: [],
+    };
+  },
+  methods: {
+    onCreate() {},
+    onDelete() {},
+    onUpdate() {},
+    onSubmit() {
+      console.log("submit!");
+    },
+    onReset() {
+      this.asset = {
+        employeeId: "",
+        assetNo: "",
+        assetSerialNumber: "",
+        assetName: "",
+        createTime: "",
+        enabled: "",
+      };
+    },
+  },
+};
 </script>
 
 <style>
