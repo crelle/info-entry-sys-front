c9def225023c0e6bde9edc7c67acc0c7c748a092
tangziyang <2313066082@qq.com>
2023-04-21 18:40:52 +0800

    工龄分布

--- src/views/app/large-screen/components/largeScreen.vue
+++ src/views/app/large-screen/components/largeScreen.vue
@@ -12,7 +12,7 @@
           <!-- 左侧中 -->
           <div class="grid-content ageCom bg-purple">
             <!-- <span>年龄分布</span> -->
-            <age-com class="ageCount"></age-com>
+            <age-com class="ageCount" :aName="ageName" :aValue="ageValue"  v-if="aa"></age-com>
           </div>
           <!-- 左侧下 -->
           <div class="grid-content bg-purple">
@@ -22,7 +22,7 @@
         <!-- 中间地图 -->
         <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
           <div class="grid-content bg-purple-light">
-            <china></china>
+            <!-- <china></china> -->
           </div>
           <div class="grid-content bg-purple-chart">
             <span>图表</span>
@@ -31,8 +31,8 @@
         <!-- 右侧内容 -->
         <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
           <!-- 右侧上 -->
-          <div class="grid-content bg-purple">
-            <span>工龄分布</span>
+          <div class="grid-content bg-purple workCom" >
+            <age-com class="ageCount" :aName="workName" :aValue="workValue"  v-if="bb" ></age-com>
           </div>
           <!-- 右侧中 -->
           <div class="grid-content bg-purple">
@@ -51,42 +51,108 @@
 <script>
 import China from "./china-map.vue";
 import ageCom from "./ageCom.vue";
-import { employeeAge,employeeSeniority } from "@/api/largeScreen";
+import { employeeAge, employeeSeniority } from "@/api/largeScreen";
 export default {
   name: "index",
   components: {
-    China,ageCom
+    China,
+    ageCom,
   },
   data() {
     return {
-      ageData:{
-      departmentName: '',
-      regionName: ''
-      }
-    }
+      ageData: {
+        departmentName: "",
+        regionName: "",
+      },
+      woekAgeData:{
+        departmentName: "",
+        regionName: "",
+      },
+      ageName:[],
+      ageValue:[],
+      aa:0,
+      bb:0,
+      workName:[],
+      workValue:[]
+    };
   },
   //菜单生成部分
-  computed: {
-
-  },
+  computed: {},
   watch: {
     // 侦听路由 路由改变则联动菜单
   },
   mounted() {
-    this.ageDeal(this.ageData)
+    this.ageDeal(this.ageData);
+    this.workAge(this.woekAgeData)
   },
 
   methods: {
-    ageDeal(age){
-      employeeAge(age).then(res=>{
-        console.log(res);
+    ageDeal(age) {
+      employeeAge(age).then((res) => {
+        if (res && res.code && res.code === "00000") {
+          let dataArr = {};
+          res.data.forEach((item) => {
+            let name = String(item.birthdayYear).substr(2, 1) + "0后";
+            dataArr[name] = dataArr[name] ? dataArr[name] + item.num : item.num;
+          });
+          this.ageName=Object.keys(dataArr).reverse()
+          this.ageValue=Object.values(dataArr).reverse()
+          // this.ageValue=this.ageFirst(Object.values(dataArr)).reverse()
+          this.aa=1
+          // console.log( this.$refs.ageCom);
+          // this.$refs.ageCom.test()
+        }
+      });
+    },
+    // ageFirst(data) {
+    //   const total = data.reduce((acc, val) => acc + val, 0);
+    //   const percentages = data.map((val) => Math.round((val / total) * 100));
+    //   let sumPercentages = percentages.reduce((acc, val) => acc + val, 0);
+    //   while (sumPercentages !== 100) {
+    //     const maxPercentage = Math.max(...percentages);
+    //     const maxPercentageIndex = percentages.indexOf(maxPercentage);
+    //     percentages[maxPercentageIndex] -= sumPercentages - 100;
+    //     sumPercentages = percentages.reduce((acc, val) => acc + val, 0);
+    //   }
+    //   return percentages;
+    // },
+    workAge(data){
+      employeeSeniority(data).then(res=>{
+        if (res && res.code && res.code === "00000") {
+          let workData={'1年以下':0,'1-2年':0,'2-3年':0,'3-4年':0,'4-5年':0,'5年以上':0}
+          res.data.forEach(item=>{
+            if(item.seniority==0){
+              workData['1年以下']+=item.num
+            }
+            if(item.seniority==1){
+              workData['1-2年']+=item.num
+            }
+            if(item.seniority==2){
+              workData['2-3年']+=item.num
+            }
+            if(item.seniority==3){
+              workData['3-4年']+=item.num
+            }
+            if(item.seniority==4){
+              workData['4-5年']+=item.num
+            }
+            if(item.seniority>=5){
+              workData['5年以上']+=item.num
+            }
+
+          })
+          this.workName=Object.keys(workData)
+          this.workValue=Object.values(workData)
+          this.bb=1
+          console.log('workAge',this.workName,this.workValue);
+        }
+        
       })
     }
-  }
+  },
 };
 </script>
 <style lang='less'>
-
 </style>
 <style lang='less' scoped>
 body,
@@ -136,18 +202,25 @@ html {
     height: 25vh;
     // background-color: rgba(224, 236, 211, 0.2);
   }
-  .ageCom{
-  background-image: url('./images/uihomepage/age.png');
-  background-size: contain;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  padding-top: 10%;
+  .ageCom {
+    background-image: url("./images/uihomepage/age.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    padding-top: 10%;
+  }
+  .workCom{
+    background-image: url("./images/uihomepage/work.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    padding-top: 10%;
+  }
+  .ageCount {
+    width: 90%;
+    height: 80%;
+  }
 }
-.ageCount{
-  width: 80%;
-  height: 80%;
-}
-}
-
 </style>
